91ace80ca7d7159fd4ba440ad58b46ae63b09d7f
Maksim <hormax14@gmail.com>
2023-08-03 23:10:47 +0300

    -render new(callback, subscribe, observer)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {rerenderEntireTree} from "./render";
-import state from './redux/state';
+import state, { subscribe } from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { addPost, updateNewPostText } from './redux/state'; 
+import {BrowserRouter} from "react-router-dom";
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+let rerenderEntireTree = (state) => {
+  
+  root.render(
+  <React.StrictMode>
+    <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+  </React.StrictMode>
+);
+}
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree)
+
 
 //https://bit.ly/CRA-vitals
 reportWebVitals();
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import { rerenderEntireTree } from "../render";
+let rerenderEntireTree = () => {
+    console.log('State was changed');
+}
 
 
 let state = {
@@ -32,7 +34,7 @@ let state = {
                 }
 }
 
-export let addPost = () => {
+export const addPost = () => {
     let newPost = {
         id: 5, message: state.profilePage.newPostText, likesCount: 0
     };
@@ -41,9 +43,13 @@ export let addPost = () => {
     rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
+}
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
 }
 
 export default state;
