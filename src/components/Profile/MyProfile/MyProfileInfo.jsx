20ba432dd4dc9e33415566cbc037ecbc9e612213
hhormax <hormax14@gmail.com>
2023-09-26 23:37:44 +0300

    my profile local state

--- src/components/Profile/MyProfile/MyProfileInfo.jsx
+++ src/components/Profile/MyProfile/MyProfileInfo.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from './MyProfileInfo.module.css';
+import ProfileStatus from "./ProfileStatus";
 
 const MyProfileInfo = (props) => {
   return (
@@ -8,9 +9,9 @@ const MyProfileInfo = (props) => {
           <img className={s.ava} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqI1ZZFfoilOlrVEwpgiCPe3ImRTeAsrNsUw&usqp=CAU"></img>
         </div>
         <div className={s.descriptionBlock}>
-          
           <div className={s.name}>{props.login}</div>
         </div>
+        <ProfileStatus status={"xdfggf"}/>
     </div>
   );
 };
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,20 +1,7 @@
 import React from "react";
 import Preloader from "../../common/Preloader/Preloader";
 import s from './ProfileInfo.module.css';
-
-// const ProfileInfo = () => {
-//   return (
-//     <div className={s.profileInfo}>
-//         <div>
-//           <img className={s.ava} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqI1ZZFfoilOlrVEwpgiCPe3ImRTeAsrNsUw&usqp=CAU"></img>
-//         </div>
-//         <div className={s.descriptionBlock}>
-//           <div className={s.name}>Name</div>
-//           <div className={s.lastName}>Lastname</div>
-//         </div>
-//     </div>
-//   );
-// };
+import ProfileStatus from '../MyProfile/ProfileStatus';
 
 const ProfileInfo = (props) => {
   if(!props.profile){
